c2a731ef5d3759cb521c245d65b3dc1d1ed33c0e
brnmrss <142026241+brnmrss@users.noreply.github.com>
2023-08-26 18:31:30 -0700

    Update delet_aluno.cpp

--- BRUNO/delet_aluno.cpp
+++ BRUNO/delet_aluno.cpp
@@ -1,14 +1,39 @@
-system("cls");
+                                system("cls");
+                                 cout << "\nAlunos Cadastrados: "<< totalAlunos <<" \n";
+                                for (int i = 0; i < totalAlunos; i++) {
+                                    cout << "Aluno " << i + 1 << ":\n";
+                                    cout << "Nome: " << alunosCadastrados[i].nome << "\t";
+                                    cout << "Email: " << alunosCadastrados[i].email << "\t";
+                                    cout << "Matricula: " << alunosCadastrados[i].matricula << "\t";
+                                    cout << "Telefone: " << alunosCadastrados[i].fone << "\t";
+                                    cout << "Turma/Serie: " << alunosCadastrados[i].turmaSerie << "\t";
+
+                                    cout << "\n---------------------------------------------------------------\n";
+                                }
+
                                 cout << "\n*********************  DELETAR CADASTRO DE ALUNO ************************ \n";
                                 cout << "Informe a matrícula do aluno que deseja deletar.\n";
-                                cout << "\nMatrícula: \n";
+                                cout << "\nMatrícula: ";
                                 cin >> alunoDel;
 
-                                for (int i = 0; i < count; i++){
-                                    if (alunoDel == Alu[i].matricula){
-                                        if((i+1) < count){
-                                        Alu[i] = Alu[i+1];
+                                int novoTotalAlunos = 0; // Armazena o total de alunos após a exclusão
+
+                                    // Criar um novo array temporário
+                                    Aluno novosAlunosCadastrados[ALUNO];
+
+                                    for (int i = 0; i < totalAlunos; i++) {
+                                        if (alunoDel != alunosCadastrados[i].matricula) {
+                                            novosAlunosCadastrados[novoTotalAlunos] = alunosCadastrados[i];
+                                            novoTotalAlunos++;
                                         }
                                     }
-                                }
-                                count--;
+
+                                    if (novoTotalAlunos < totalAlunos) {
+                                        // Exclusão bem-sucedida, atualizar o array principal
+                                        totalAlunos = novoTotalAlunos;
+                                        for (int i = 0; i < totalAlunos; i++) {
+                                            alunosCadastrados[i] = novosAlunosCadastrados[i];
+                                        }
+                                    } else {
+                                        cout << "Aluno não encontrado para exclusão." << endl;
+                                    }
